edf4fb17b38b1f3f8a79a2ef9739c1fa633c24d8
Ben Howdle <benhowdle89@gmail.com>
2017-09-16 16:21:17 +0100

    add twitter oauth

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 const http = require('http')
-const oauth = require('oauth')
+const OAuth = require('oauth').OAuth
 
-const { PORT = 3000 } = process.env
+if (!process.env.PRODUCTION) {
+  require('dotenv').config()
+}
+
+const {
+  PORT = 3000,
+  TWITTER_CONSUMER_KEY,
+  TWITTER_CONSUMER_SECRET,
+  TWITTER_CALLBACK_URL
+} = process.env
 
 http.createServer((req, res) => {
+  const oauth = new OAuth(
+    "https://api.twitter.com/oauth/request_token",
+    "https://api.twitter.com/oauth/access_token",
+    TWITTER_CONSUMER_KEY,
+    TWITTER_CONSUMER_SECRET,
+    "1.0A",
+    TWITTER_CALLBACK_URL,
+    "HMAC-SHA1"
+  )
+
+  oauth.getOAuthRequestToken((error, oauth_token, oauth_token_secret) => {
+    
+  })
+  
   res.end('Hello World from Node 8\n')
 }).listen(PORT)
 
