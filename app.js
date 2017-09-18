ceb1cdd0574bacf0af729f6c6a5c4b512c839c73
Ben Howdle <benhowdle89@gmail.com>
2017-09-18 20:39:21 +0100

    make twitter class

--- app.js
+++ app.js
@@ -1,34 +1,25 @@
 const http = require('http')
-const Promise = require('bluebird')
-const OAuth = Promise.promisifyAll(require('oauth'), {
-  multiArgs: true
-}).OAuth
 
 if (!process.env.PRODUCTION) {
   require('dotenv').config()
 }
 
+const Twitter = require('./lib/twitter')
+const response = require('./lib/response')
+
 const {
-  PORT = 3000,
-  TWITTER_CONSUMER_KEY,
-  TWITTER_CONSUMER_SECRET,
-  TWITTER_CALLBACK_URL
+  PORT = 3000
 } = process.env
 
 http.createServer(async (req, res) => {
-  const oauth = new OAuth(
-    "https://api.twitter.com/oauth/request_token",
-    "https://api.twitter.com/oauth/access_token",
-    TWITTER_CONSUMER_KEY,
-    TWITTER_CONSUMER_SECRET,
-    "1.0A",
-    TWITTER_CALLBACK_URL,
-    "HMAC-SHA1"
-  )
-
-  const [ oAuthToken, oAuthTokenSecret ] = await oauth.getOAuthRequestTokenAsync()
   
-  res.end('Hello World from Node 8\n')
+  const twitter = new Twitter()
+  const { data, errors } = await twitter.getOAuthRequestToken()
+  return res.end(response({
+    data,
+    errors
+  }))
+
 }).listen(PORT)
 
 console.log('listening on', PORT)
