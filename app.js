bd92013c2c2d9eeef6571cb0b776ba287d608a3c
Ben Howdle <benhowdle89@gmail.com>
2017-09-18 22:05:24 +0100

    cors

--- app.js
+++ app.js
@@ -11,7 +11,22 @@ const {
   PORT = 3000
 } = process.env
 
+const setCORSHeaders = res => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Request-Method', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+	res.setHeader('Access-Control-Allow-Headers', '*');
+}
+
 http.createServer(async (req, res) => {
+
+  setCORSHeaders(res)
+
+  if (req.method === 'OPTIONS') {
+		res.writeHead(200)
+		res.end()
+		return
+	}
 
   const router = new Router()
   const { data, errors } = await router.handle(req)
