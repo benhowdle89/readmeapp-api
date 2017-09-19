88edfb484b3308031baca8ff0e37394bf9f3fa65
Ben Howdle <benhowdle89@gmail.com>
2017-09-19 21:49:29 +0100

    cors, accesstoken, body parse

--- app.js
+++ app.js
@@ -1,40 +1,38 @@
 const http = require('http')
+const jsonBody = require("body/json")
 
 if (!process.env.PRODUCTION) {
   require('dotenv').config()
 }
 
 const Router = require('./lib/router')
+const CORS = require('./lib/cors')
 const response = require('./lib/response')
 
 const {
   PORT = 3000
 } = process.env
 
-const setCORSHeaders = res => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Request-Method', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-	res.setHeader('Access-Control-Allow-Headers', '*');
-}
-
 http.createServer(async (req, res) => {
-
-  setCORSHeaders(res)
-
-  if (req.method === 'OPTIONS') {
-		res.writeHead(200)
-		res.end()
-		return
-	}
-
-  const router = new Router()
-  const { data, errors } = await router.handle(req)
-  return res.end(response({
-    data,
-    errors
-  }))
-
+  jsonBody(req, res, async (err, body) => {
+    CORS(res)
+    
+      if (req.method === 'OPTIONS') {
+        res.writeHead(200)
+        res.end()
+        return
+      }
+    
+      const router = new Router()
+      const {
+        data = {},
+        errors = {}
+      } = await router.handle(req, body)
+      return res.end(response({
+        data,
+        errors
+      }))
+  })
 }).listen(PORT)
 
 console.log('listening on', PORT)
